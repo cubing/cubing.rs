6b39929f6db3d855ed90912f902197ceb91cb919
Lucas Garron <code@garron.net>
2023-12-01 02:14:32 -0800

    [kpuzzle] Restore macros, this time under `cubing::kpuzzle`.

--- cubing/src/lib.rs
+++ cubing/src/lib.rs
@@ -1,11 +1,13 @@
 pub mod alg {
     pub use cubing_core::alg::*;
-    pub use cubing_macros::parse_alg;
-    pub use cubing_macros::parse_move;
+    pub use cubing_macros::{parse_alg, parse_move};
 }
 
 pub mod kpuzzle {
     pub use cubing_core::kpuzzle::*;
+    pub use cubing_core::{
+        kpattern_from_json_file, kpuzzle_from_json_file, ktransformation_from_json_file,
+    };
 }
 
 pub mod puzzles {
