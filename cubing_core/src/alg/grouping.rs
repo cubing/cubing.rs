49b8add7afd72442ffe7bd06c6cb7c8d863b4a15
Lucas Garron <code@garron.net>
2024-11-01 12:49:11 -0700

    [alg] Fix Square-1 tuple serialization and inversion.

--- cubing_core/src/alg/grouping.rs
+++ cubing_core/src/alg/grouping.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 
 use super::amount::fmt_amount;
 use super::special_notation::{D_SQ_quantum, U_SQ_quantum};
-use super::{Alg, AlgNode};
+use super::{Alg, AlgNode, Move};
 
 // TODO: Remove `PartialEq` if we add any metadata (e.g. parsing info, or memoizations).
 #[derive(Debug, Clone, PartialEq)]
@@ -14,10 +14,31 @@ pub struct Grouping {
 
 impl Grouping {
     pub fn invert(&self) -> Grouping {
+        if let Some((move_0, move_1)) = self.square1_tuple() {
+            let nodes = vec![move_0.invert().into(), move_1.invert().into()];
+            let alg = Arc::new(Alg { nodes });
+            return Self { alg, amount: 1 };
+        }
         Grouping {
             alg: self.alg.clone(),
             amount: -self.amount,
         }
+    }
+
+    fn square1_tuple(&self) -> Option<(&Move, &Move)> {
+        if self.alg.nodes.len() == 2 && self.amount == 1 {
+            // Square-1 notation
+            if let AlgNode::MoveNode(move_0) = &self.alg.nodes[0] {
+                if move_0.quantum == U_SQ_quantum() {
+                    if let AlgNode::MoveNode(move_1) = &self.alg.nodes[1] {
+                        if move_1.quantum == D_SQ_quantum() {
+                            return Some((move_0, move_1));
+                        }
+                    }
+                }
+            }
+        }
+        None
     }
 }
 
@@ -31,17 +52,9 @@ impl fmt::Display for Grouping {
                 self.alg.nodes[0],
                 super::AlgNode::CommutatorNode(_) | super::AlgNode::ConjugateNode(_)
             )
-        } else if self.alg.nodes.len() == 2 {
+        } else if let Some((move_0, move_1)) = self.square1_tuple() {
             // Square-1 notation
-            if let AlgNode::MoveNode(move_0) = &self.alg.nodes[0] {
-                if move_0.quantum == U_SQ_quantum() {
-                    if let AlgNode::MoveNode(move_1) = &self.alg.nodes[1] {
-                        if move_1.quantum == D_SQ_quantum() {
-                            return write!(f, "({}, {})", move_0.amount, move_1.amount);
-                        }
-                    }
-                }
-            }
+            return write!(f, "({}, {})", move_0.amount, move_1.amount);
         }
         if include_parentheses {
             write!(f, "(")?;
